b04aededc576b2fcdb81eadd387716ddf89378ff
luisg210 <gerardo.henriquez21@itca.edu.sv>
2024-10-20 15:08:00 -0600

    otro

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/luisg210/notes",
   plugins: [react(), svgrPlugin()],
 }); 
