6e1e69fd63129e2b4ac62bea4b8d0a76eae9c838
luisg210 <gerardo.henriquez21@itca.edu.sv>
2024-10-20 14:48:27 -0600

    Otro

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,20 @@
 import axios from 'axios';
 //import { getEnvVariable } from '../helpers';
 
-    
-//const API_URL = 'http://localhost:5000/api';
 const API_URL = 'https://api-lulu-notes.onrender.com/api/';
 
-const api = axios.create({      
-    baseURL: API_URL   
+const api = axios.create({
+    baseURL: API_URL
 });
-   
-api.interceptors.request.use( config => {    
-    config.headers = {    
+
+api.interceptors.request.use(config => {
+    config.headers = {
         ...config.headers,
-        'x-token': localStorage.getItem('x-token'),           
+        'x-token': localStorage.getItem('x-token'),
         'user-id': localStorage.getItem('user-id')
     }
-  
-    return config;    
+
+    return config;
 });
 
 export default api;
