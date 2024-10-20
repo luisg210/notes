048daef1acd63d94803256647a76a48379864aa9
luisg210 <gerardo.henriquez21@itca.edu.sv>
2024-10-20 14:57:54 -0600

    Pushing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://luisg210.github.io/notes/",
+  base: "https://github.com/luisg210/notes",
   plugins: [react(), svgrPlugin()],
-});
+}); 
